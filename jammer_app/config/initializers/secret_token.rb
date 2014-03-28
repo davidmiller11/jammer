9f7c9fe09bb876ce8808b361e330e1a6e0c8505c
davidmiller11 <davidmiller11@gmail.com>
2014-03-28 11:32:36 -0400

    initialized jammer_app

---
+++ jammer_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JammerApp::Application.config.secret_key_base = '0904351653f8d47c9523b15754fea4dc1ce7d9e4f35fc7d186fb7d186149b2b1cbd1c659f835a030d616d87df4dd07b9f7b35f269f13941fb834d08885cd1d27'
---
+++ jammer_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JammerApp::Application.config.session_store :cookie_store, key: '_jammer_app_session'
