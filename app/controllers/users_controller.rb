b1fa6146e37893fdf10b5291fa2a5bfbb2aa903c
davidmiller11 <davidmiller11@gmail.com>
2014-04-03 20:53:51 -0400

    take out admin option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,7 +68,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 
 end
