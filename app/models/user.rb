f1591a90c0f53918404c2fae208997b3a2ffd390
davidmiller11 <davidmiller11@gmail.com>
2014-04-09 16:30:50 -0400

    hi

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,21 +38,6 @@ class User < ActiveRecord::Base
 
   # returns an array of all jams a user has been invited to
   def jams_invited_to
-
-    # get array of all rsvps for user
-    @rsvps = Rsvp.where(user_id: self.id)
-
-    # get array of all jam_times for user
-    @jam_times = @rsvps.map do |rsvp|
-      JamTime.find(rsvp.jam_time_id)
-    end
-
-    # get array of all jams user was invited to
-    @jams = @jam_times.map do |jam_time|
-      Jam.find(jam_time.jam_id)
-    end
-
-    # return array of all jams with duplicates removed.
-    return @jams.uniq
+    return Rsvp.where(user_id: self.id).map {|rsvp| rsvp.jam_time.jam }.uniq
   end
 end
